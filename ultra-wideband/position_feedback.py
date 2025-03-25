7095292a7063a3c70a9b4b68b36f26c39f3bc660
colbren <colfujimoto@gmail.com>
2025-03-25 12:32:13 -1000

    Working implementation of ultra-wideband position feedback without position commands

--- ultra-wideband/position_feedback.py
+++ ultra-wideband/position_feedback.py
@@ -1,4 +1,3 @@
-
 import serial
 import time
 import datetime
@@ -14,29 +13,31 @@ time.sleep(1)
 DWM.write("lec\r".encode())
 print("Encode")
 time.sleep(1)
-
 try:
     while True:
         data = DWM.readline()
+        print(data)
+        data = data.decode("utf-8").strip()
         if(data):
             print(data)
             if ("DIST" in data and "AN0" in data and "AN1" in data and "AN2" in data):
                 data = data.replace("\r\n", "")
-                data = data.decode().split(",")
-                if("DIST" in data):
-                    anchor_Nummber = int(data[data.index("DIST")+1])
-                    for i in range(anchor_Nummber):
-                        pos_AN = {"id": data[data.index("AN"+str(i))+1], "x": data[data.index("AN"+str(i))+2], "y": data[data.index(
-                            "AN"+str(i))+3], "dist": data[data.index("AN"+str(i))+5]}
-                        pos_AN = json.dumps(pos_AN)
-                        print(pos_AN)
-                        r.set('AN'+str(i), pos_AN)
-                if("POS" in data):
-                    pos = {"x": data[data.index("POS")+1],
-                           "y": data[data.index("POS")+2]}
-                    pos = json.dumps(pos)
-                    print(pos)
-                    r.set("pos", pos)
+                data = data.split(",")  # Removed the unnecessary decode
+            if("DIST" in data):
+                anchor_Number = int(data[data.index("DIST")+1])
+                for i in range(anchor_Number):
+                    pos_AN = {"id": data[data.index("AN"+str(i))+1], "x": data[data.index("AN"+str(i))+2], "y": data[data.index(
+                        "AN"+str(i))+3], "dist": data[data.index("AN"+str(i))+5]}
+                    pos_AN = json.dumps(pos_AN)
+                    print(pos_AN)
+                    r.set('AN'+str(i), pos_AN)
+            if("POS" in data):
+                pos = {"x": data[data.index("POS")+1],
+                   "y": data[data.index("POS")+2]}
+                pos = json.dumps(pos)
+                print(pos)
+                r.set("pos", pos)
+
     DWM.write("\r".encode())
     DWM.close()
 
